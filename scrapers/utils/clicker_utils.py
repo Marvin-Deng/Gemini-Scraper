f44e780f07355e3545196bf6215b37a03cc8c35f
HathawayQAQ <94154940+HathawayQAQ@users.noreply.github.com>
2024-04-18 01:59:51 -0400

    Comments removed

--- scrapers/utils/clicker_utils.py
+++ scrapers/utils/clicker_utils.py
@@ -13,14 +13,6 @@ from .wait_utils import (
 )
 
 def wait_element_visible_by_id(element_id: str, driver: WebDriver) -> object:
-    """
-    Wait for the element with the specified ID to be visible and return it.
-    Parameters:
-    element_id (str): ID of the element.
-    driver (WebDriver): The WebDriver instance.
-    Returns:
-    WebElement: The WebElement instance that is visible.
-    """
     return WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.ID, element_id))
     )
