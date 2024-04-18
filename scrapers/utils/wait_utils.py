a219fb2f6fa6afd62773947002e17af2558fd525
HathawayQAQ <94154940+HathawayQAQ@users.noreply.github.com>
2024-04-17 23:47:10 -0400

    Create wait_utils visible_id

--- scrapers/utils/wait_utils.py
+++ scrapers/utils/wait_utils.py
@@ -17,3 +17,18 @@ def wait_elements_visible_by_css(css_selector: str, driver: WebDriver) -> list:
     return WebDriverWait(driver, 10).until(
         EC.visibility_of_all_elements_located((By.CSS_SELECTOR, css_selector))
     )
+
+def wait_element_visible_by_id(element_id: str, driver: WebDriver) -> object:
+    """
+    Wait for the element with the specified ID to be visible and return it.
+
+    Parameters:
+    element_id (str): ID of the element.
+    driver (WebDriver): The WebDriver instance.
+
+    Returns:
+    WebElement: The WebElement instance that is visible.
+    """
+    return WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, element_id))
+    )
